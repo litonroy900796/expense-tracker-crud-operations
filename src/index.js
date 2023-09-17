8a51db57e8a2c53499f6c7d84ce997323753c7eb
litonroy900796e <litonroy900796@gmail.com>
2023-09-18 00:46:06 +0600

    server add .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { store } from "./apps/store";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
@@ -9,7 +11,9 @@ const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
+        <Provider store={store}>
             <App />
+        </Provider>
     </React.StrictMode>
 );
 
